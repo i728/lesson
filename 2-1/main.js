108de672d45b7bf8cb7d42de7e4929dace6dc747
i728 <shunsei728@icloud.com>
2021-07-08 14:39:33 +0900

    add 2-1

--- 2-1/main.js
+++ 2-1/main.js
@@ -1,4 +1,8 @@
 let one = process.argv[2];
 let two = process.argv[3];
 let onetwo = one/two*100;
-console.log(onetwo + "%");
+if(isNaN(onetwo) || onetwo == 'Infinity'){
+    console.log('Error: Arguments');
+}else{
+    console.log(onetwo + "%");
+}
